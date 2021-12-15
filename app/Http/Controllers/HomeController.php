b2d46ac140fdd5510047531477f31e35b7b49749
ARDIANSYAH PUTRA <xcz.ardiansyahputra2468@gmail.com>
2021-12-15 13:11:21 +0800

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('test.index');
+        return view('index');
     }
 
     public function home()
